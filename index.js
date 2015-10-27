d41fc8717156dbfde5e218a99f95860b7cb6cd6b
Rafał Pocztarski <r.pocztarski@gmail.com>
2015-10-27 12:46:18 +0100

    Add initial version

---
+++ index.js
@@ -0,0 +1,32 @@
+
+"use strict";
+
+module.exports = tco;
+
+function tco(f) {
+
+    if (typeof f == 'function') {
+        var tf = function () {
+            var nf = f, na = arguments, next = true;
+            while (1) {
+                var r = nf.apply(null, na);
+                if (typeof r[0] == 'function') {
+                    if (typeof r[0].tco == 'function') {
+                        nf = r[0].tco;
+                        na = r[1];
+                    } else {
+                        return r[0].apply(null, r[1]);
+                    }
+                } else if (r[0] == null) {
+                    return r[1];
+                } else {
+                    throw new Error('tco: bad value returned');
+                }
+            }
+        };
+        tf.tco = f;
+        return tf;
+    } else {
+        throw new Error('tco() expects a function');
+    }
+}
